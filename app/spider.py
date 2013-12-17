6b95b874b105d974cf9ecca7cd03dcd24e813cac
GuoQi <qiguo@hustunique.com>
2013-12-17 21:07:42 +0800

    简单的爬虫，用来爬取指定URL的title和description

---
+++ app/spider.py
@@ -0,0 +1,27 @@
+import urllib2
+import re
+
+class DOM():
+	__url__ = None
+	title = None
+	content = None
+	def __init__(self, url):
+		# 这里需要加url检验, 并且能够转换为标准的url
+		self.__url__ = url
+
+	def getTitle(self):
+		request = urllib2.Request(self.__url__)
+		response = urllib2.urlopen(request)
+		dom = response.read().decode('utf-8')
+
+		title_pattern = re.compile('<title>.*</title>?', re.S)
+
+		match = title_pattern.findall(dom)
+
+		result = []
+		for value in match:
+			tmp_str = value[(value.find('<title>') + len('<title>')) : value.find('</title>')]
+			result.append(tmp_str)
+
+		return result[0]
+
