66761413602ad12d5227d8afa4c4fe6cfbcb3005
Weijian Yang <ywj200844@gmail.com>
2013-12-21 20:52:33 +0800

    almost complete the banner in index.html

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,22 +2,111 @@ var bannerMoveLeftButton  = document.getElementsByClassName("bannerMoveLeftButto
 var bannerMoveRightButton = document.getElementsByClassName("bannerMoveRightButton")[0];
 var bannerContentShow     = document.getElementsByClassName("bannerContentShow")[0];
 var bannerContent         = document.getElementsByClassName("bannerContent")[0];
+var bannerOrder = document.getElementsByClassName("bannerOrder")[0];
 var bannerMoveJudge       = 1;
 var bannerMoveAutoRestart = 1;
 var bannerMoveAutoTime;
 
+
+bannerOrder.children[0].onclick = function(){
+	if(bannerMoveJudge){
+		var bannerOrderNow = (parseInt(bannerContentShow.style.marginLeft)||0)/-100;
+		console.log("test0:"+(0 - bannerOrderNow));
+		if((0 - bannerOrderNow)>0){
+			moveLeft(75,0 - bannerOrderNow);
+		}
+		else if((0 - bannerOrderNow)<0){
+				moveRight(75,bannerOrderNow - 0);
+			};
+		clearInterval(bannerMoveAutoTime);
+		bannerMoveAuto();
+	};
+};
+
+bannerOrder.children[1].onclick = function(){
+	if(bannerMoveJudge){
+		var bannerOrderNow = (parseInt(bannerContentShow.style.marginLeft)||0)/-100;
+		console.log("test1:"+(1 - bannerOrderNow));
+		if((1 - bannerOrderNow)>0){
+			moveLeft(75,1 - bannerOrderNow);
+		}
+		else if((1 - bannerOrderNow)<0){
+				moveRight(75,bannerOrderNow - 1);
+			};
+		clearInterval(bannerMoveAutoTime);
+		bannerMoveAuto();
+	};
+};
+
+bannerOrder.children[2].onclick = function(){
+	if(bannerMoveJudge){
+		var bannerOrderNow = (parseInt(bannerContentShow.style.marginLeft)||0)/-100;
+		console.log("test2:"+(2 - bannerOrderNow));
+		if((2 - bannerOrderNow)>0){
+			moveLeft(75,2 - bannerOrderNow);
+		}
+		else if((2 - bannerOrderNow)<0){
+				moveRight(75,bannerOrderNow - 2);
+			};
+		clearInterval(bannerMoveAutoTime);
+		bannerMoveAuto();
+	};
+};
+
+bannerOrder.children[3].onclick = function(){
+	if(bannerMoveJudge){
+		var bannerOrderNow = (parseInt(bannerContentShow.style.marginLeft)||0)/-100;
+		console.log("test3:"+(3 - bannerOrderNow));
+		if((3 - bannerOrderNow)>0){
+			moveLeft(75,3 - bannerOrderNow);
+		}
+		else if((3 - bannerOrderNow)<0){
+				moveRight(75,bannerOrderNow - 3);
+			};
+		clearInterval(bannerMoveAutoTime);
+		bannerMoveAuto();
+	};
+};
+
+bannerOrder.children[4].onclick = function(){
+	if(bannerMoveJudge){
+		var bannerOrderNow = (parseInt(bannerContentShow.style.marginLeft)||0)/-100;
+		console.log("test4:"+(4 - bannerOrderNow));
+		if((4 - bannerOrderNow)>0){
+			moveLeft(75,4 - bannerOrderNow);
+		}
+		else if((4 - bannerOrderNow)<0){
+				moveRight(75,bannerOrderNow - 4);
+			};
+		clearInterval(bannerMoveAutoTime);
+		bannerMoveAuto();
+	};
+};
+
 bannerMoveLeftButton.onclick = function(){
 	if(bannerMoveJudge){
-		moveRight(75);
+		var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft)||0;
+		if( whereBeforeMove<0){
+			moveRight(75);
+		}
+		else{
+			moveLeft(75,4);
+		}
 		clearInterval(bannerMoveAutoTime);
 		bannerMoveAuto();
 	};
 };
 
 bannerMoveRightButton.onclick = function(){
-	clearInterval(bannerMoveAutoTime);;
+	clearInterval(bannerMoveAutoTime);
 	if(bannerMoveJudge){
-		moveLeft(75);
+		var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft)||0;
+		if( whereBeforeMove>-400){
+			moveLeft(75);
+		}
+		else{
+			moveRight(75,4)
+		}
 		clearInterval(bannerMoveAutoTime);
 		bannerMoveAuto();
 	};
@@ -40,7 +129,13 @@ window.onload = function(){
 var bannerMoveAuto = function(){
 	bannerMoveAutoTime = setInterval(function(){
 		if(bannerMoveJudge){
-			moveLeft(50);
+			var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft)||0;
+			if( whereBeforeMove>-400){
+				moveLeft(50);
+			}
+			else{
+				moveRight(50,4)
+			}
 		};
 	},3000);
 };
@@ -55,37 +150,26 @@ var moveLeft = function(T){
 	var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft)||0;
 	var moveDistance = 0;
 	bannerMoveJudge = 0;
-	if( whereBeforeMove>-400){
-		var t = setInterval(function(){
-			
-			if(countTime <= t1){
-				moveDistance = 0.5*a1*countTime*countTime;
-				bannerContentShow.style.marginLeft = (whereBeforeMove - moveDistance)+"%";
-			}
-			else{
-				moveDistance = 40+v*(countTime-t1)-0.5*a2*(countTime-t1)*(countTime-t1);
-				bannerContentShow.style.marginLeft = (whereBeforeMove - moveDistance)+"%";
-			};
-			//console.log(countTime+":"+moveDistance);
-			if(countTime < T){
-				++countTime;
-			}
-			else{
-				clearInterval(t);
-				bannerMoveJudge = 1;
-			}
-		},1);
+	var moveLength;
+	if(arguments.length == 2){
+		moveLength = arguments[1]
 	}
 	else{
+		moveLength = 1;
+	};
+		var bannerOrderNum = whereBeforeMove/(-100);
+		console.log(bannerOrderNum);
+		bannerOrder.children[bannerOrderNum].style.cssText = "";
+		bannerOrder.children[bannerOrderNum+moveLength].style.cssText = "background-color:white !important";
 		var t = setInterval(function(){
 			
 			if(countTime <= t1){
 				moveDistance = 0.5*a1*countTime*countTime;
-				bannerContentShow.style.marginLeft = (whereBeforeMove + 4*moveDistance)+"%";
+				bannerContentShow.style.marginLeft = (whereBeforeMove - moveLength*moveDistance)+"%";
 			}
 			else{
 				moveDistance = 40+v*(countTime-t1)-0.5*a2*(countTime-t1)*(countTime-t1);
-				bannerContentShow.style.marginLeft = (whereBeforeMove + 4*moveDistance)+"%";
+				bannerContentShow.style.marginLeft = (whereBeforeMove - moveLength*moveDistance)+"%";
 			};
 			//console.log(countTime+":"+moveDistance);
 			if(countTime < T){
@@ -96,7 +180,6 @@ var moveLeft = function(T){
 				bannerMoveJudge = 1;
 			}
 		},1);
-	};			
 }
 
 var moveRight = function(T){
@@ -106,40 +189,29 @@ var moveRight = function(T){
 	const t1 = 0.4*T;
 	const t2 = 0.6*T;
 	const v  = 200/T;
-	var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft);
+	var bannerOrderNum;
+	var whereBeforeMove = parseInt(bannerContentShow.style.marginLeft)||0;
 	var moveDistance = 0;
 	bannerMoveJudge = 0;
-	if( whereBeforeMove<0){
-		var t = setInterval(function(){
-			
-			if(countTime <= t1){
-				moveDistance = 0.5*a1*countTime*countTime;
-				bannerContentShow.style.marginLeft = (whereBeforeMove + moveDistance)+"%";
-			}
-			else{
-				moveDistance = 40+v*(countTime-t1)-0.5*a2*(countTime-t1)*(countTime-t1);
-				bannerContentShow.style.marginLeft = (whereBeforeMove + moveDistance)+"%";
-			};
-			//console.log(countTime+":"+moveDistance);
-			if(countTime < T){
-				++countTime;
-			}
-			else{
-				clearInterval(t);
-				bannerMoveJudge = 1;
-			}
-		},1);
+	var moveLength;
+	if(arguments.length == 2){
+		moveLength = arguments[1]
 	}
 	else{
+		moveLength = 1;
+	};
+		var bannerOrderNum = whereBeforeMove/(-100);
+		bannerOrder.children[bannerOrderNum].style.cssText = "";
+		bannerOrder.children[bannerOrderNum-moveLength].style.cssText = "background-color:rgb(213,213,213) !important";
 		var t = setInterval(function(){
 			
 			if(countTime <= t1){
 				moveDistance = 0.5*a1*countTime*countTime;
-				bannerContentShow.style.marginLeft = (whereBeforeMove - 4*moveDistance)+"%";
+				bannerContentShow.style.marginLeft = (whereBeforeMove + moveLength*moveDistance)+"%";
 			}
 			else{
 				moveDistance = 40+v*(countTime-t1)-0.5*a2*(countTime-t1)*(countTime-t1);
-				bannerContentShow.style.marginLeft = (whereBeforeMove - 4*moveDistance)+"%";
+				bannerContentShow.style.marginLeft = (whereBeforeMove + moveLength*moveDistance)+"%";
 			};
 			//console.log(countTime+":"+moveDistance);
 			if(countTime < T){
@@ -150,5 +222,4 @@ var moveRight = function(T){
 				bannerMoveJudge = 1;
 			}
 		},1);
-	};	
 };
