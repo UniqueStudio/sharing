fd5678856ab23ffc833db4644fb1ee9f14d1b820
GuoQi <qiguo@hustunique.com>
2013-12-31 20:20:00 +0800

    init mail

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ from flask.ext.mail import Mail
 from config import mail_config
 
 app = Flask(__name__)
-mail = Mail(app)
 app.config.from_object('config')
 app.config.update(mail_config)
+mail = Mail(app)
 db = SQLAlchemy(app)
 
 from app import views, models, forms, mail
