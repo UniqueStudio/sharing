f07a1f8654f925c5a2f1ed3e760e0b7ece8ef121
GuoQi <qiguo@hustunique.com>
2014-02-15 14:50:34 +0800

    修正auth里的一个bug，修正db_create.py关于应用上下文的bug

--- db_create.py
+++ db_create.py
@@ -2,6 +2,9 @@
 '''
 该文件用来创建数据库
 '''
-from app import db
-db.drop_all()
-db.create_all()
+from app.models import db
+from app import app
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
--- sharing.sql
+++ sharing.sql
@@ -24,15 +24,16 @@ DROP TABLE IF EXISTS `comment`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `body` varchar(528) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `share_id` int(11) DEFAULT NULL,
+  `body` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `share_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `share_id` (`share_id`),
   CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`share_id`) REFERENCES `share` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +42,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,'zhangsheng:sdf',1,9),(2,'zhangsheng:adsf',1,9),(3,'zhangsheng对@zhangsheng说:asd',1,9),(4,'guoqi:adsf',1,7),(5,'guoqi对@guoqi说:sdf',1,7);
+INSERT INTO `comment` VALUES (1,'blablabla...','2014-02-14 07:24:13',2,1),(2,'这个段子真不错','2014-02-07 05:03:06',2,2),(3,'hello','2014-02-06 00:00:00',1,2);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +61,7 @@ CREATE TABLE `group` (
   UNIQUE KEY `title` (`title`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `group_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +70,6 @@ CREATE TABLE `group` (
 
 LOCK TABLES `group` WRITE;
 /*!40000 ALTER TABLE `group` DISABLE KEYS */;
-INSERT INTO `group` VALUES (3,'hello',1),(4,'abc',1),(5,'haha',1),(6,'dsfg',1);
 /*!40000 ALTER TABLE `group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,6 @@ CREATE TABLE `group_users` (
 
 LOCK TABLES `group_users` WRITE;
 /*!40000 ALTER TABLE `group_users` DISABLE KEYS */;
-INSERT INTO `group_users` VALUES (3,1),(3,2),(4,2),(5,1);
 /*!40000 ALTER TABLE `group_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,16 +108,16 @@ DROP TABLE IF EXISTS `share`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `share` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `title` varchar(180) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `explain` varchar(350) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `likes` int(11) DEFAULT NULL,
-  `timestamp` datetime DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `explain` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `likes` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `share_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +126,7 @@ CREATE TABLE `share` (
 
 LOCK TABLES `share` WRITE;
 /*!40000 ALTER TABLE `share` DISABLE KEYS */;
-INSERT INTO `share` VALUES (1,'http://blog.csdn.net/shell_picker/article/details/6033023','Vim 的 tab 设置 - Dreamer - 博客频道 - CSDN.NET','explain',1,'2013-12-25 20:43:41',1),(2,'http://www.pythonclub.org/linux/vim/tabs','VIM 标签页 (tab) [Python俱乐部]','explain',0,'2013-12-25 20:43:46',1),(3,'http://www.cmd5.com/','md5在线查询破解,md5解密加密','explain',1,'2013-12-25 20:43:53',1),(4,'http://www.oschina.net/news/26227/github-blocked','github 在多地被墙 - 开源中国社区','explain',0,'2013-12-26 19:11:48',1),(5,'https://github.com/UniqueStudio/sharing','UniqueStudio/sharing','explain',0,'2013-12-26 19:11:54',1),(6,'https://www.openshift.com/forums/openshift/why-i-always-get-error-in-git-clone','Why I always get error in git clone ? | OpenShift by Red Hat','explain',0,'2013-12-26 19:12:16',1),(7,'https://www.openshift.com/forums/openshift/why-i-always-get-error-in-git-clone','Why I always get error in git clone ? | OpenShift by Red Hat','explain',0,'2013-12-26 19:12:22',1),(8,'http://www.google.com.hk/','Google','sharing reason',0,'2013-12-31 15:49:21',2),(9,'http://deerchao.net/tutorials/regex/regex.htm','正则表达式30分钟入门教程','sharing reason',1,'2013-12-31 15:49:30',2);
+INSERT INTO `share` VALUES (1,'hello world','这是一个测试','http://www.baidu.com/',0,'2014-02-04 10:18:47',1),(2,'hello','这是另一个测试','http://www.baidu.com/',2,'2014-02-04 12:13:57',1),(3,'nihao','这是第三个测试','http://www.baidu.com/',1,'2014-02-04 07:37:09',2),(4,'world','这是第四个测试','http://localhost:5000/',2,'2014-02-04 23:59:59',1),(5,'guoqi','blablabla....','http://www.csdn.com',1,'2014-02-02 03:07:19',2),(6,'hello','test','http://www.baidu.com/',0,'2014-02-09 23:26:34',1);
 /*!40000 ALTER TABLE `share` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,12 +139,13 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `pwdhash` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pwdhash` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nickname` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +154,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'guoqi','uniqueguoqi@gmail.com','202cb962ac59075b964b07152d234b70'),(2,'zhangsheng','shengzhang@hustunique.com','202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` VALUES (1,'uniqueguoqi@gmail.com','c81e728d9d4c2f636f067f89cc14862c','guoqi',NULL),(2,'767813944@qq.com','c81e728d9d4c2f636f067f89cc14862c','hello',NULL),(4,'abc@gmai.com','202cb962ac59075b964b07152d234b70','hello',NULL),(5,'abc@gmail.com','202cb962ac59075b964b07152d234b70','hello ',NULL),(7,'qiguo@hustunique.com','202cb962ac59075b964b07152d234b70','郭旗','https://lh4.googleusercontent.com/-kr7Fvx_vFTM/AAAAAAAAAAI/AAAAAAAAAA8/foybQAaCUNU/photo.jpg?sz=50');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,7 +181,7 @@ CREATE TABLE `user_likes` (
 
 LOCK TABLES `user_likes` WRITE;
 /*!40000 ALTER TABLE `user_likes` DISABLE KEYS */;
-INSERT INTO `user_likes` VALUES (1,1),(1,3),(1,9);
+INSERT INTO `user_likes` VALUES (1,2),(2,2),(1,3),(1,4),(2,4),(2,5);
 /*!40000 ALTER TABLE `user_likes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -194,4 +194,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-01-19 20:53:56
+-- Dump completed on 2014-02-15 14:46:56
