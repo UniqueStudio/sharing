0c5376c93476bdf51205e136eca4c9a67711b66d
BingLau7 <binglau1819@gmail.com>
2015-04-12 15:08:52 +0800

    构造接口初步完成

--- application/models/models.py
+++ application/models/models.py
@@ -11,19 +11,46 @@ class Comment(Document):
     content = StringField(required=True)
     create_time = DateTimeField(required=True, default=datetime.datetime.now)
     share = ReferenceField('Share')
+    is_delete = BooleanField(required=True, default=False)  #删除标记
+
+    @classmethod
+    def is_exist(cls, id):
+        return Comment.objects(id=id).first() is not None
 
 class Share(Document):
+    """
+        区分是否是同一个share的方法是判断url和own_group是否相等
+    """
     title = StringField(required=True)
     explain = StringField(required=True)
-    url = URLField(required=True, unique=True)
-    own_group = ListField(ReferenceField('ShareGroup'), default=[])
-    share_users = ListField(ReferenceField('User'), default=[])
+    url = URLField(required=True)
+    own_group = ReferenceField('ShareGroup')
+    share_users = ListField(ReferenceField('User'), default=[]) #单个组分析用户
     share_time = DateTimeField(required=True, default=datetime.datetime.now)
     gratitude_num = IntField(required=True, default=0)
 
     gratitude_users = ListField(ReferenceField('User'), default=[])
     comments = ListField(ReferenceField(Comment))
 
+    @classmethod
+    def is_exist(cls, url, group):    #是否在group中存在url的share
+        return Share.objects(url=url, own_group=group).first() is not None
+
+    def gratitude(self, user):
+        self.gratitude_num += 1
+        self.gratitude_users.append(user)
+        self.save()
+
+    def add_share(self, user, group):
+        if not Share.is_exist(self.url, group):
+            self.share_users.append(user)
+            self.own_group = group
+            self.save()
+
+    def remove_share_user(self, user, group):   #删除分享的用户
+        if Share.is_exist(self.url, group):
+            self.share_users.remove(user)
+            self.save()
 
 class ShareGroup(Document):
     name = StringField(required=True, unique=True)
@@ -34,11 +61,33 @@ class ShareGroup(Document):
     shares = ListField(ReferenceField(Share))
     users = ListField(ReferenceField('User'), default=[])
 
+    @classmethod
+    def is_exist(cls, name):
+        groups = cls.objects(name=name).first()
+        return groups != None
+
+    def is_admin(self, user):
+        return user in self.administrators
+
+    def add_user(self, user):   #用户加入group中，逻辑上应该是group做的事
+        if User.is_exist(user.email) and not user.is_in_the_group(self) \
+                and ShareGroup.is_exist(self.name):
+            user.groups.append(self)
+            self.users.append(user)
+            self.save()
+            user.save()
+
+    def remove(self, user):
+        """
+            这个方法仅是从group删除用户，具体是管理员删除或是用户自行退组不管
+        """
+        self.users.remove(user)
+        self.save()
 
 class User(Document):
     email = EmailField(required=True, unique=True)
     password = StringField(required=True)
-    nickname = StringField(required=True, unique=True)
+    nickname = StringField(required=True)
     register_time = DateTimeField(required=True, default=datetime.datetime.now())
     avatar = StringField(required=True, default='have no')
 
@@ -47,25 +96,127 @@ class User(Document):
     education_information = StringField()
     brief = StringField()
     inviter = ReferenceField('self')
+
     self_shares = ListField(ReferenceField(Share), default=[])
     gratitude_shares = ListField(ReferenceField(Share), default=[])
+
     comments = ListField(ReferenceField(Comment), default=[])
     black_users = ListField(ReferenceField('self'), default=[])
     attention_users = ListField(ReferenceField('self'), default=[])
+
     groups = ListField(ReferenceField(ShareGroup), default=[])
+
     manager_groups = ListField(ReferenceField(ShareGroup), default=[])
+
+
+    def __repr__(self):
+        return '<User :%s\n email:%s>' % (self.nickname, self.email)
 
     @classmethod
     def is_exist(cls, email):
         users = cls.objects(email=email)
         return users.count() > 0
 
+    #组部分
     def is_in_the_group(self, group):
         return group in self.groups
 
-    def add_the_group(self, group):
-        if not self.is_in_the_group(group):
-            self.groups.append(group)
+    def remove_the_group(self, group):  #退组,与管理员身份无关
+        if self.is_in_the_group(group)and ShareGroup.is_exist(group.name):
+            self.groups.remove(group)
+            group.users.remove(self)
+            #如果是管理员也删除
+            if self in group.administrators:
+                group.administrators.remove(self)
+                group.save()
+            self.save()
+
+    #感谢部分
+    def is_gratitude(self, share):
+        return share in self.gratitude_shares
+
+    def gratitude(self, share, group):     #感谢分享到group的share
+        pass
+
+    #个人分享部分
+    def is_share(self, share, group):   #是否分享到某个组
+        return share in self.self_shares and share.own_group == group
+
+    def share_to_group(self, share, group):     #将share分享到group中
+        pass
+
+    def remove_share_to_group(self, share, group):  #从group删除分享了的share
+        pass
+
+    #添加评论部分
+    def add_comment_to_share(self, share, comment):
+        """
+            向某个share(每个group的share在数据库表现是独立的)添加comment
+        """
+        pass
+
+    def remove_comment_to_share(self, share, comment):
+        """
+            向某个share(每个group的share在数据库表现是独立的)删除comment
+        """
+        pass
+
+    #关注拉黑部分
+    def add_attention(self, user):
+        if User.is_exist(user.mail) and user not in self.attention_users:
+            self.remove_block(user)
+            self.attention_users.append(user)
+
+    def block(self, user):
+        if User.is_exist(user.mail) and user not in self.black_users:
+            self.remove_attention(user)
+            self.black_users.append(user)
+
+    def remove_attention(self, user):
+        if User.is_exist(user.mail) and user in self.attention_users:
+            self.attention_users.remove(user)
+
+    def remove_block(self, user):
+        if User.is_exist(user.mail) and user in self.black_users:
+            self.black_users.remove(user)
+
+    #修改个人信息部分
+    def modify_gender(self, is_man):
+        self.is_man = is_man
+
+    def modify_brief(self, brief):
+        self.brief = brief
+
+    def modify_education_information(self, education_information):
+        self.education_information = education_information
+
+    def modify_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    #以下均为管理员方法，具体是否整理为单独一个类后行考虑
+    def is_admin(self, group):
+        """
+            是否是group的管理员
+        """
+        pass
+
+    def remove_group(self, user, group):
+        """
+            从group中删除user
+        """
+        pass
+
+    def remove_share_group(self, share, group):
+        """
+            删除该用户在group中分享的share
+        """
+        pass
+
+    def add_user(self, user, group):
+        """
+            使user加入group
+        """
+        pass
 
 if __name__ == '__main__':
     from mongoengine import connect
@@ -86,7 +237,7 @@ if __name__ == '__main__':
     # #测试添加share
     # share = Share(title='test', explain='test', url='http://www.baidu.com').save()
     # group.shares.append(share)
-    # share.own_group.append(group)
+    # share.own_group=group
     # share.share_users.append(user1)
     # user1.self_shares.append(share)
     # #测试share加评论，comment的user和share指定
@@ -111,6 +262,13 @@ if __name__ == '__main__':
     # share.save()
     # comment.save()
 
-    user = User.objects(email='498283580@qq.com').first()
-    group = ShareGroup.objects(name='test').first()
-    print user.is_in_the_share_group(group)
+    # user = User.objects(email='498283581@qq.com').first()
+    # share = Share.objects(title='test').first()
+    # print Share.is_exist(share)
+    # comment = Comment.objects(user=user).first()
+    # print Comment.is_exist(comment)
+    # group = ShareGroup.objects(name='test').first()
+    # print user.is_in_the_share_group(group)
+    # print user.is_gratitude(share)
+
+    #print ShareGroup.is_exists('tet')
