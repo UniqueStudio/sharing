7df48b62cacc7024293fcb34251fbc90b3ee00d0
chenzhangyu <chenzhangyu.hust.cn@gmail.com>
2015-07-05 11:35:43 +0800

    Prepare for online testing

--- README.md
+++ README.md
@@ -6,4 +6,3 @@
 
 ##安装
 `pip install -r request.txt`
-因为版本兼容[问题](https://github.com/MongoEngine/mongoengine/issues/935), 其中PyMongo==2.8
--- manager.py
+++ manager.py
@@ -11,7 +11,7 @@ from application import Application
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(5000)
+    http_server.listen(8887)
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
